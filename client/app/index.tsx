02db41d174354c7f08c6fdf29ff6cbc77dd58f12
Ashutosh Naik <ashutoshnaik360@gmail.com>
2024-12-29 22:47:58 +0530

    commit

---
+++ client/app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+import "./global.css";
+export default function Index() {
+  return (
+    <View className="flex justify-center items-center">
+      <Text className="w-full text-2xl">Hello world</Text>
+    </View>
+  );
+}
