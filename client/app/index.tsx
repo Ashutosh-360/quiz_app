d2f5f1ac0b0d9e07fe48eea10b01d2a9678d7b27
Ashutosh Naik <ashutoshn@advantageclub.co>
2024-12-30 13:24:19 +0530

    tailwind

--- client/app/index.tsx
+++ client/app/index.tsx
@@ -1,6 +1,7 @@
 import { Text, View } from "react-native";
 import "../global.css";
 export default function Index() {
+  console.log("");
   return (
     <View className="text-2xl font-semibold flex justify-center items-center flex-1">
       <Text className="">Edit </Text>
