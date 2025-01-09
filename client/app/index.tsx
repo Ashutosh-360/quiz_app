ffb185d61767d02b8e728135b132743955954494
Ashutosh Naik <ashutoshnaik360@gmail.com>
2025-01-09 23:39:18 +0530

    code

--- client/app/index.tsx
+++ client/app/index.tsx
@@ -1,22 +1,22 @@
 import React, { useContext } from "react";
-import { NavigationProvider, useNavigation } from "../Utilities/Context/NavigationContext.js";
+import {
+  NavigationProvider,
+  useNavigation,
+} from "../Utilities/Context/NavigationContext.js";
 import Login from "../app/loginPage";
 import SignUp from "../app/signupPage";
-import "../global.css"
-import QuizList from '../app/quizListing/index.js'
-
+import "../global.css";
+import QuizList from "../app/quiz_listing/index.js";
 
 const MainApp = () => {
   const { currentScreen } = useNavigation();
-  console.log("jey",currentScreen)
   switch (currentScreen) {
-   
     case "Login":
       return <Login />;
     case "SignUp":
       return <SignUp />;
-    case "QuizListing":
-      return <QuizList/>;
+    case "quiz_listing":
+      return <QuizList />;
     default:
       return <Login />;
   }
